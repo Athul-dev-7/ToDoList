7c8516cb2e607f437851790de1a59d2260bc1ae4
Athul Das <athuldas7777@gmail.com>
2021-07-17 12:39:56 +0530

    stopped tracking .env

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+
+
+
+
+SECRET_KEY = 'django-insecure-&=m$k9x2yvpzuy=(1*nn188w2rytzc!q9q@mzi8)t&d6*ct3-k'
+
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
